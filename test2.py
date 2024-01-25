6d6935fa05192e81020e1fa813d11ebc4ebc17d7
ZxcTimurka <timurerofeev22@gmail.com>
2024-01-25 20:24:22 +0700

    123

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 import math
 import random
+import sys
 
 import pygame
 import pymunk
@@ -7,15 +8,38 @@ import pymunk.pygame_util
 
 
 # Загрузка изображения
-def load_image(radius, image='imgs/smile.jpg'):
+def load_image(radius, image='imgs/smile.png'):
     ball_image = pygame.image.load(image)
     ball_image = pygame.transform.scale(ball_image, (radius * 2, radius * 2))
     return ball_image
 
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
 # Переменная для отслеживания количества шаров
 ball_count = 0
 sp_coord = (0, 0)
+
+
+def start_screen():
+    intro_text = ["Перемещение героя", '',
+                  "Герой двигается",
+                  "Карта на месте"]
+    fon = pygame.transform.scale(pygame.image.load('imgs/fon.png'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
 
 
 # Функция для создания шарика
@@ -26,6 +50,7 @@ def create_ball(space, pos, radius):
     inertia = pymunk.moment_for_circle(mass, 10, radius, (0, 0))
     body = pymunk.Body(mass, inertia)
     body.position = pos
+    body.time = 0
     shape = pymunk.Circle(body, radius, (0, 0))
     shape.elasticity = 0.4
     shape.friction = 0.5
@@ -54,6 +79,10 @@ def remove_ball(arbiter, space, data):
     if isinstance(arbiter.shapes[0], pymunk.Circle) and isinstance(arbiter.shapes[1], pymunk.Circle):
         shape = arbiter.shapes[0]
         shape2 = arbiter.shapes[1]
+        if shape.radius == max(radius) and shape2.radius == max(radius):
+            space.remove(shape, shape.body)
+            space.remove(shape2, shape2.body)
+            print('you win')
         if shape.radius == shape2.radius and shape.radius != max(radius):
             ball_count -= 2
             create_ball(space, (shape2.body.position.x, shape2.body.position.y), shape.radius + 10)
@@ -65,10 +94,11 @@ def remove_ball(arbiter, space, data):
 
 
 def main():
-    global radius, ball_count
+    global radius, ball_count, screen, width, height
     pygame.init()
     width, height = 600, 600
     screen = pygame.display.set_mode((width, height))
+    start_screen()
     clock = pygame.time.Clock()
     space = pymunk.Space()
     space.gravity = (0, 500)
@@ -85,7 +115,9 @@ def main():
     for l in static_lines:
         l.friction = 0.5
     space.add(*static_lines)
+
     random_rad = min(radius)
+
     running = True
     while running:
         screen.fill('white')
@@ -110,11 +142,17 @@ def main():
         pygame.draw.line(screen, 'black', (0, height - 1), (width, height - 1), 5)
         pygame.draw.line(screen, 'black', (1, 1), (1, height), 5)
         pygame.draw.line(screen, 'black', (width - 1, 1), (width - 1, height), 5)
+        pygame.draw.line(screen, 'red', (0, 100), (width, 100), 5)
 
         # Отрисовка изображения на шарике
 
         for ball in space.shapes:
             if isinstance(ball, pymunk.shapes.Circle):
+                if ball.body.position.y < 100:
+                    ball.body.time += 1
+                    if ball.body.time > 100:
+                        quit()
+
                 w = h = ball.radius * 2
                 image = load_image(ball.radius, dic[ball.radius])
                 blitRotate(screen, image, ball.body.position, (w / 2, h / 2), -ball.body.angle * 180 / math.pi)
